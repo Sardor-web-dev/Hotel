dcae0c98d6d62b36139ce82ef30e6d793d5dd059
Sardor-web-dev <dzamolovsardor5@gmail.com>
2026-03-05 19:49:16 +0500

    done all pages

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,18 @@
+import ContactHeader from "@/components/custom/ContactHeader";
+import Footer from "@/components/custom/Footer";
+import ContactSection from "@/containers/ContactSection";
+import HomeSection from "@/containers/MainSection";
+
+export default function Contactpage() {
+  return (
+    <div>
+      <div>
+        <div className="flex flex-col items-center gap-10 justify-center">
+          <ContactHeader/>
+          <ContactSection />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+}
